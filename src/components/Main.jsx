34db3b032a68eac65dbf2b65864c4fc8e9015ec9
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-01-30 16:40:57 +0300

    storage eklendi.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
 import Form from "./Form";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/config";
+import Spinner from "./Spinner";
+import Post from "./Post";
+
 const Main = ({ user }) => {
+  const tweetsCol = collection(db, "tweets");
+
+  const [tweets, setTweets] = useState(null);
+  useEffect(() => {
+    const unsub = onSnapshot(tweetsCol, (snapshot) => {
+      const tempTweets = [];
+
+      snapshot.forEach((doc) => tempTweets.push({ id: doc.id, ...doc.data() }));
+      setTweets(tempTweets);
+    });
+    return () => unsub();
+  }, []);
+
   return (
     <div className="border border-gray-700 overflow-y-auto p-6">
       <header className="font-bold p-4 border-b-[1px] border-gray-700">
@@ -8,6 +27,11 @@ const Main = ({ user }) => {
       <Form user={user} />
 
       {/* tweet listeleme */}
+      {!tweets ? (
+        <Spinner />
+      ) : (
+        tweets.map((tweet) => <Post key={tweet.id} tweet={tweet} />)
+      )}
     </div>
   );
 };
