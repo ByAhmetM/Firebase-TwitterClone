648ce91a6231a9048c080635d1f53f610670b66f
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-02-01 01:39:23 +0300

    aside için toplam tweet hesaplandı. ekrana basıldı

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,24 @@
+import { collection, count, getAggregateFromServer } from "firebase/firestore";
+import { db } from "../firebase/config";
+import { useEffect, useState } from "react";
+
 const Aside = () => {
-  return <div className="max-lg:hidden">Aside</div>;
+  const [data, setData] = useState(null);
+  const tweetCol = collection(db, "tweets");
+
+  useEffect(() => {
+    getAggregateFromServer(tweetCol, {
+      tweetsCount: count(),
+    }).then((data) => setData(data._data));
+  }, []);
+
+  return (
+    <div className="max-lg:hidden">
+      <p className="my-5 text-center p-3 text-lg">
+        Toplam Tweet Sayısı: {data?.tweetsCount?.integerValue}
+      </p>
+    </div>
+  );
 };
 
 export default Aside;
