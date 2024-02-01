96466b71302ee9664791a01be2150a6d636c94c5
ByAhmetM <ahmetmuratunsal@gmail.com>
2024-02-01 16:54:26 +0300

    reset

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -5,12 +5,9 @@ import { auth } from "./../firebase/config";
 
 const ProtectedRoute = () => {
   const [isAuth, setIsAuth] = useState(null);
-  const [kullanici, setKullanici] = useState("");
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
-      localStorage.setItem("TOKEN", user.uid);
-      setKullanici(user.uid);
       if (user) {
         setIsAuth(true);
       } else {
@@ -20,9 +17,7 @@ const ProtectedRoute = () => {
     return () => unsub();
   }, []);
 
-  const activeUserToken = localStorage.getItem("TOKEN");
-  console.log(kullanici);
-  if (isAuth === false && activeUserToken != kullanici) {
+  if (isAuth === false) {
     return <Navigate to={"/"} replace />;
   }
 
